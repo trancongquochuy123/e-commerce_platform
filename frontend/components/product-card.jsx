96cb5d14a00b1e94ab970bebce43afa7bf33a6ba
SunGoat1001 <ngoctien10012004@gmail.com>
2025-12-13 19:59:00 +0700

    optimize image load over above

--- frontend/components/product-card.jsx
+++ frontend/components/product-card.jsx
@@ -14,6 +14,7 @@ export default function ProductCard({ product }) {
         width={500}
         height={500}
         className="aspect-square rounded-lg bg-gray-200 object-cover group-hover:opacity-75"
+        priority
       />
       <div className="h-12">
         <span className="text-lg text-gray-700">{product.name}</span>
